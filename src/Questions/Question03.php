37b33b9fd96b397fd8c08760105b20e6c370afa2
Luiz Fernando <cruzfernando.dev@gmail.com>
2026-06-22 20:01:29 -0300

    feat: implementando a questao 03

---
+++ src/Questions/Question03.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Questions;
+
+class Question03
+{
+    public function title(): string
+    {
+        return 'Questão 03 - Validação de Expressões';
+    }
+
+    public function description(): string
+    {
+        return 'Durante o cadastro de fórmulas em um sistema financeiro, é necessário garantir que os parênteses informados estejam corretamente balanceados.'
+            . ' Implemente uma função que valide uma sequência de parênteses.';
+    }
+
+    public function example(): string
+    {
+        return "Exemplos:\n✓ '((()))' => válido\n✗ '(()))' => inválido";
+    }
+
+    public function input(): array
+    {
+        return [
+            '(()())',
+            '((())',
+            '(()))',
+            '()()()',
+            '((()))',
+            '(()()',
+        ];
+    }
+
+    public function execute(): array
+    {
+        $inputs = $this->input();
+        $results = [];
+
+        foreach ($inputs as $input) {
+            $results[] = $this->isValidParentheses($input) ? 'válido' : 'inválido';
+        }
+
+        return $results;
+    }
+
+    /**
+     * Valida se uma sequencia de parenteses esta balanceada.
+     *
+     * @param string $s A sequencia de parenteses a ser validada.
+     * @return bool Retorna true se a sequencia estiver balanceada, caso contrario, false.
+     */
+    private function isValidParentheses(string $s): bool
+    {
+        $stack = [];
+
+        for ($i = 0; $i < strlen($s); $i++) {
+            $char = $s[$i];
+
+            if ($char === '(') {
+                array_push($stack, $char);
+            } else {
+                if (empty($stack)) {
+                    return false;
+                }
+                array_pop($stack);
+            }
+        }
+
+        return empty($stack);
+    }
+
+    public function status(): string
+    {
+        return 'Resolvido';
+    }
+}
